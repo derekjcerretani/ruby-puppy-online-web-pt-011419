198f4b608c13692a5454d65253eb35f3e5a5a1c2
Derek Cerretani <derekcerretani@gmail.com>
2019-02-05 21:09:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,11 +12,11 @@ class Dog
   end
 
   def self.clear_all
-    @@all = []
+    @@all.clear
   end
 
   def self.all
-    @@all.each do |name|
+    @@all.each do |dog|
       puts dog.name
     end
   end

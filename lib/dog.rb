4ee80ab2d0bb9faa858c3279cf44e1b95398295f
Derek Cerretani <derekcerretani@gmail.com>
2019-02-05 21:03:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,14 +8,12 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    @@all << @name
   end
 
   def self.clear_all
     @@all = []
   end
-
-#binding.pry
 
   def self.all
     @@all.each do |@name|
